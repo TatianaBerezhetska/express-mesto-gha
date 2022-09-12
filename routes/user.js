d3f74b877dbc4464049b1e64a0899929bc79013d
Tatiana Berezhetskaya <t.berezhetskaya@gmail.com>
2022-09-12 12:59:26 +1000

    hotfixes after autotests

--- routes/user.js
+++ routes/user.js
@@ -13,18 +13,18 @@ const {
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 router.get('/:userId', celebrate({
-  params: Joi.object.keys({
+  params: Joi.object().keys({
     userId: Joi.string().alphanum().length(24),
   }),
 }), getUser);
 router.patch('/me', celebrate({
-  body: Joi.object.keys({
+  body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 router.patch('/me/avatar', celebrate({
-  body: Joi.object.keys({
+  body: Joi.object().keys({
     avatar: Joi.string().required().pattern(urlRegExp),
   }),
 }), updateAvatar);
